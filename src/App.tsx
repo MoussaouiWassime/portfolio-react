def8839b2890d5d2d706dc32a447eddfa053906a
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-01-21 15:27:34 +0100

    feat : add styling for App component to enhance layout structure

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Footer from './components/Footer';
 function App() {
     return (
         <div>
+        <div className="relative min-h-screen flex flex-col selection:bg-orange-500/30 selection:text-orange-200">
+
             <div className="relative z-10 flex flex-col flex-1 pb-10">
                 <Navbar />
                 <Hero />
