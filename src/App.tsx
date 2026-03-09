ed98e725f526e0a5c6d24d6847b89746d71d078b
Wassime Moussaoui <wassime.moussaoui@etudiant.univ-reims.fr>
2026-03-09 16:41:09 +0100

    feat: add ScrollToTop component for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
     return (
@@ -25,6 +26,7 @@ function App() {
             <div className="relative z-10">
                 <Footer />
             </div>
+            <ScrollToTop />
         </div>
     );
 }
